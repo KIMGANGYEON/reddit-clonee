90bbae8e52ff4d8c2fed3651caf3f0f6e9e523aa
KIMGANGYEON <rkddus3177@naver.com>
2024-09-27 14:21:06 +0900

    컴터를 몇 번 초기화 하는 거야

---
+++ front/pages/_app.tsx
@@ -0,0 +1,48 @@
+import NavBar from "@/components/NavBar";
+import { AuthProvider } from "@/context/auth";
+import "@/styles/globals.css";
+import axios from "axios";
+import Axios from "axios";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { SWRConfig } from "swr";
+
+export default function App({ Component, pageProps }: AppProps) {
+  Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
+  Axios.defaults.withCredentials = true;
+
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
+
+  const fetcher = async (url: string) => {
+    try {
+      const res = await axios.get(url);
+      return res.data;
+    } catch (error: any) {
+      throw error.response.data;
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <script
+          defer
+          src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"
+          integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <SWRConfig value={{ fetcher }}>
+        <AuthProvider>
+          {!authRoute && <NavBar />}
+          <div className={authRoute ? "" : "pt-16"}>
+            <Component {...pageProps} />
+          </div>
+        </AuthProvider>
+      </SWRConfig>
+    </>
+  );
+}
---
+++ front/pages/index.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import useSWR from "swr";
+import { Sub } from "../types";
+import axios from "axios";
+import Image from "next/image";
+import { useAuthState } from "@/context/auth";
+
+export default function Home() {
+  const { authenticated } = useAuthState();
+
+  const address = "http://localhost:4000/api/subs/sub/topSubs";
+  const { data: topSubs } = useSWR<Sub[]>(address);
+
+  return (
+    <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+      <div className="w-full md:mr-3 md:w-8/12"></div>
+
+      <div className="hidden w-4/12 ml-3 md:block">
+        <div className="bg-white border rounded">
+          <div className="p-4 border-b">
+            <p className="text-lg -font-semibold text-center">상위 커뮤니티</p>
+          </div>
+          <div>
+            {topSubs?.map((sub) => (
+              <div
+                key={sub.name}
+                className="flex items-center px-4 py-2 text-xs border-b"
+              >
+                <Link href={`/r/${sub.name}`}>
+                  <span>
+                    <Image
+                      src={sub.imageUrl}
+                      alt="Sub"
+                      className="rounded-full cursor-pointer"
+                      width={24}
+                      height={24}
+                    />
+                  </span>
+                </Link>
+                <Link href={`/r/${sub.name}`}>
+                  <span className="ml-2 font-bold hover:cursor-pointer">
+                    /r/{sub.name}
+                  </span>
+                </Link>
+                <p className="ml-auto font-md">{sub.postCount}</p>
+              </div>
+            ))}
+          </div>
+
+          {authenticated && (
+            <div className="w-full py-6 text-center">
+              <Link href="/subs/create">
+                <span className="w-full p-2 text-center text-whte bg-gray-400 rounded">
+                  커뮤니티 만들기
+                </span>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
