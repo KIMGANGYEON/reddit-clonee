1d8af899e6329efcfc76749f9443a3194612c814
KIMGANGYEON <rkddus3177@naver.com>
2024-10-01 15:16:26 +0900

    ..

--- front/components/PostCard.tsx
+++ front/components/PostCard.tsx
@@ -1,12 +1,16 @@
+import { useAuthState } from "@/context/auth";
 import { Post } from "@/types";
+import axios from "axios";
 import dayjs from "dayjs";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
 interface PostCardProps {
   post: Post;
+  subMutate?: () => void;
 }
 
 const PostCard = ({
@@ -24,13 +28,32 @@ const PostCard = ({
     username,
     sub,
   },
+  subMutate,
 }: PostCardProps) => {
+  const router = useRouter();
+  const isInSubPage = router.pathname === "/r/[sub]";
+  const { authenticated } = useAuthState();
+  const vote = async (value: Number) => {
+    if (!authenticated) router.push("/login");
+
+    if (value === userVote) {
+      value = 0;
+    }
+
+    try {
+      await axios.post("/votes", { identifier, slug, value });
+      if (subMutate) subMutate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex mb-4 bg-white rounded" id={identifier}>
       <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
         <div
           className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
-          //   onClick={() => vote(1, comment)}
+          onClick={() => vote(1)}
         >
           {userVote === 1 ? (
             <FaArrowUp className="mx-auto text-red-500" />
@@ -41,7 +64,7 @@ const PostCard = ({
         <p className="text-xs font-bold">{voteScore}</p>
         <div
           className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
-          //   onClick={() => vote(-1, comment)}
+          onClick={() => vote(-1)}
         >
           {userVote === -1 ? (
             <FaArrowDown className="mx-auto text-blue-500" />
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,19 +1,44 @@
 import Link from "next/link";
 import useSWR from "swr";
-import { Sub } from "../types";
+import { Post, Sub } from "../types";
 import axios from "axios";
 import Image from "next/image";
 import { useAuthState } from "@/context/auth";
+import useSWRInfinite from "swr/infinite";
+import PostCard from "@/components/PostCard";
 
 export default function Home() {
   const { authenticated } = useAuthState();
 
   const address = "http://localhost:4000/api/subs/sub/topSubs";
+
+  const getKey = (pageIndex: number, previousPageData: Post[]) => {
+    if (previousPageData && !previousPageData.length) return null;
+    return `/posts?page=${pageIndex}`;
+  };
+
+  const {
+    data,
+    error,
+    size: page,
+    setSize: setPage,
+    isValidating,
+    mutate,
+  } = useSWRInfinite<Post[]>(getKey);
+  const isInitialLoading = !data && !error;
+  const posts: Post[] = data ? ([] as Post[]).concat(...data) : [];
   const { data: topSubs } = useSWR<Sub[]>(address);
 
   return (
     <div className="flex max-w-5xl px-4 pt-5 mx-auto">
-      <div className="w-full md:mr-3 md:w-8/12"></div>
+      <div className="w-full md:mr-3 md:w-8/12">
+        {isInitialLoading && (
+          <p className="text-lg text-center">로딩중입니다...</p>
+        )}
+        {posts?.map((post) => (
+          <PostCard key={post.identifier} post={post} />
+        ))}
+      </div>
 
       <div className="hidden w-4/12 ml-3 md:block">
         <div className="bg-white border rounded">
